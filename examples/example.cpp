e6e71526c842ec71626f7e1c7709761795c5f5c6
RaulPPealez <raulppelaez@gmail.com>
2023-03-09 10:42:09 +0100

    Add another example

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,6 +1,28 @@
-#include<libmd.h>
+/* Raul P. Pelaez 2023.
+   Basic interaction with libmd.
+ */
+#include <libmd.h>
 
-int main(){
+using namespace cl;
 
+int main() {
 
+  sycl::queue q = md::get_default_qeue();
+  // Print some properties of the sycl queue
+  md::log("Device: " + q.get_device().get_info<sycl::info::device::name>());
+  md::log("Vendor: " + q.get_device().get_info<sycl::info::device::vendor>());
+  md::log("Driver version: " +
+          q.get_device().get_info<sycl::info::device::driver_version>());
+  md::log("Max work group size: %d",
+          q.get_device().get_info<sycl::info::device::max_work_group_size>());
+  md::log(
+      "Max work item dimensions: %d",
+      q.get_device().get_info<sycl::info::device::max_work_item_dimensions>());
+  md::log("Max work item sizes: %d %d %d",
+					q.get_device().get_info<sycl::info::device::max_work_item_sizes>()[0],
+					q.get_device().get_info<sycl::info::device::max_work_item_sizes>()[1],
+					q.get_device().get_info<sycl::info::device::max_work_item_sizes>()[2]);
+  md::log("Max compute units: %d",
+	  q.get_device().get_info<sycl::info::device::max_compute_units>());
+  return 0;
 }
