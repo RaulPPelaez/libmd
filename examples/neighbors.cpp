5816fad6a51db53c45d5bf8b112ccc882f49fed8
RaulPPealez <raulppelaez@gmail.com>
2023-03-14 08:21:47 +0100

    Update example

--- examples/neighbors.cpp
+++ examples/neighbors.cpp
@@ -20,27 +20,24 @@ auto generateParticleCloud(int num_particles, float box_size) {
 }
 
 int main() {
-  auto q = md::get_default_queue();
   int num_particles = 100;
   float box_size = 16.f;
   float cutoff = 4.0f;
   auto positions = generateParticleCloud(num_particles, box_size);
   auto [neighbors, neighbor_indices, max_num_neighbors] =
-      md::computeNeighbors(q, positions, cutoff);
+      md::computeNeighbors(positions, cutoff);
   // The format of the neighbor list is as follows:
   // neighbors[i]: The number of neighbors of particle i
   // neighbor_indices[i * max_num_neighbors + j]: The index of the jth neighbor
   // of particle i max_num_neighbors: The maximum number of neighbors of any
   // particle
-  sycl::host_accessor neighbors_acc{neighbors, sycl::read_only};
-  std::cout << "Number of neighbors of particle 0: " << neighbors_acc[0]
+  std::cout << "Number of neighbors of particle 0: " << neighbors[0]
             << std::endl;
   // Print every neighbor of particle 0 in the format "Neighbors of particle 0:
   // neighbor1 neighbor2 ..."
-  sycl::host_accessor neighbor_indices_acc{neighbor_indices, sycl::read_only};
   std::cout << "Neighbors of particle 0: ";
-  for (int i = 0; i < neighbors_acc[0]; i++) {
-    std::cout << neighbor_indices_acc[i] << " ";
+  for (int i = 0; i < neighbors[0]; i++) {
+    std::cout << neighbor_indices[i] << " ";
   }
   std::cout << std::endl;
   return 0;
